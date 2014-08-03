ad73c8211722d2e64f726cbb25c3018d6afaf6e7
Paolo Laurenti <laurentipaolo@gmail.com>
2014-08-03 18:06:25 +0200

    Add beer quantity update and beers remove

--- controllers/beer.js
+++ controllers/beer.js
@@ -43,9 +43,6 @@ exports.getBeer = function (req, res) {
     db.Beer
         .find({where: {userId: req.user.id, id: req.params.beer_id }})
         .then(function (beer) {
-//            console.log(req.user);
-//            console.log('   ' + req.params);
-//            console.log('     ' + req);
             res.send(beer);
         }, function (error) {
             res.send(error);
@@ -55,9 +52,14 @@ exports.getBeer = function (req, res) {
 // Create endpoint /api/beers/:beer_id for PUT
 exports.putBeer = function (req, res) {
     db.Beer
-        .update({quantity: req.body.quantity}, {userId: req.user._id, id: req.params.beer_id})
+        .find({where: {userId: req.user.id, id: req.params.beer_id }})
         .then(function (beer) {
-            res.json({ message: num + ' updated' });
+            if (!beer)
+                res.status(404).send('Not found');
+
+            beer.quantity = req.body.quantity;
+            beer.save();
+            res.send(beer);
         }, function (err) {
             res.send(err);
         });
@@ -65,12 +67,20 @@ exports.putBeer = function (req, res) {
 
 // Create endpoint /api/beers/:beer_id for DELETE
 exports.deleteBeer = function (req, res) {
-
     db.Beer
-        .destroy({ userId: req.user._id, id: req.params.beer_id })
+        .find({where: {userId: req.user.id, id: req.params.beer_id }})
         .then(function (beer) {
-            res.json({ message: 'Beer removed from the locker!' });
-        }, function (err) {
-            res.send(err);
+            if (!beer)
+                res.status(404).send('Not found');
+
+            beer
+                .destroy()
+                .then(function () {
+                    res.json({ message: 'Beer removed from the locker!' });
+                }, function (err) {
+                    res.send(err);
+                });
+        }, function (error) {
+            res.send(error);
         });
 };
